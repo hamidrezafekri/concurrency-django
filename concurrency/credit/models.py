638d2a80947a093c0a245c8307b62d5f2d000c79
hamidrezafekr <hamidrezafekri.dev@gmail.com>
2023-12-16 16:11:18 +0330

    add product , transation , request models

--- concurrency/credit/models.py
+++ concurrency/credit/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from concurrency.common.models import BaseModel
+from concurrency.users.models import BaseUser
+from enum import IntEnum
 
-# Create your models here.
+class CreditRequestStatus(IntEnum):
+
+    PENDDING=1
+    APPROVED=2
+    REJECTED=3
+
+    @classmethod
+    def choice(cls):
+        return [(key.name , key.value) for key in cls]
+
+class TransactionType(IntEnum):
+    SELL=1
+    CREDIT=2
+
+    @classmethod
+    def choice(cls):
+        return [(key.name , key.value) for key in cls]
+
+
+class CreditRequest(BaseModel):
+    status = models.IntegerField(choices=CreditRequestStatus.choice() , default=CreditRequestStatus.PENDDING)
+    amount = models.DecimalField(max_digits=15,
+                                 decimal_places=2,
+                                 )
+    seller= models.ForeignKey(to=BaseUser , on_delete= models.CASCADE,
+                              related_name="creditrequest")
+
+    def __str__(self):
+        return f"{self.seller.lastname}-{self.amount}-{self.status}"
+
+
+
+class Transaction(BaseModel):
+    transaction_type = models.IntegerField(
+            choices=TransactionType.choice(),
+            )
+    amount = models.DecimalField(max_digits=15,
+                                 decimal_places=2,
+                                 )
+    seller = models.ForeignKey(
+            BaseUser,
+            on_delete = models.SET_NULL,
+            related_name="transactions_as_seller",
+            null=True,
+            blank=True,
+            )
+    customer =  models.ForeignKey(
+            BaseUser,
+            on_delete = models.SET_NULL,
+            null= True,
+            blank = True,
+            related_name = "transactions_as_customer",
+            )
+    seller_new_balance = models.DecimalField(
+            max_digits=15,
+            decimal_places=2,
+            )
+    customer_new_balance = models.DecimalField(
+            max_digits=15,
+            decimal_places = 2,
+            )
+                               
+    product = models.ForeignKey("Product",
+                                on_delete=models.SET_NULL,
+                                null = True,
+                                blank = True
+                            )
+class Product(BaseModel):
+    amount = models.DecimalField(
+            max_digits=15,
+            decimal_places=2,
+            )
+    seller = models.ForeignKey(BaseUser , on_delete=models.CASCADE,
+                               related_name="products")
+    is_active = models.BooleanField()
+
