cf09b7cc6786fdb8b28217856d44b43575e39dc2
hamidrezafekr <hamidrezafekri.dev@gmail.com>
2023-12-25 18:47:35 +0330

    change how to work with locust

--- concurrency/loadtest/auth.py
+++ concurrency/loadtest/auth.py
@@ -1,22 +1,19 @@
-from locust import HttpUser, task
-
-
-
-
-
-
-
+from locust import HttpUser, task, between
 
 
 class AdminUser(HttpUser):
+    wait_time = between(1, 5)
 
     def on_start(self):
-        response = self.client.post("/api/auth/jwt/admin-login" ,
-                         json= {"phone_number":"09123456789",
-                                "password":"@hamid14520"})
-        print(response.content)
-
+        response = self.client.post("/api/auth/jwt/admin-login/",
+                                    json={"phone_number": "09123456789",
+                                          "password": "@hamid14520"})
+        self.token = self.token = response.json()['token']['access']
+        self.headers = {"Authorization": f"Bearer {self.token}"}
 
     @task
     def get_user(self):
-        self.client.get('/api/user/user')
+        self.client.get('/api/user/user/', headers=self.headers)
+
+
+
