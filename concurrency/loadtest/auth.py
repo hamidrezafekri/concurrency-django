9bf977890940b3f3898910be4407c1469fb825f4
hamidrezafekr <hamidrezafekri.dev@gmail.com>
2023-12-28 05:37:54 +0330

    add test for login customer , seller and admin users

--- concurrency/loadtest/auth.py
+++ concurrency/loadtest/auth.py
@@ -16,4 +16,36 @@ class AdminUser(HttpUser):
         self.client.get('/api/user/user/', headers=self.headers)
 
 
+class SellerUser(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        response = self.client.post("/api/auth/jwt/seller-login/",
+                                    json={"phone_number": "09101111111",
+                                          "password": "@hamid14520"})
+        self.token = self.token = response.json()['token']['access']
+        self.headers = {"Authorization": f"Bearer {self.token}"}
+
+    @task
+    def get_user(self):
+        self.client.get('/api/user/user/', headers=self.headers)
+
+
+
+class CustomerUser(HttpUser):
+    wait_time = between(1, 5)
+
+    def on_start(self):
+        response = self.client.post("/api/auth/jwt/login/",
+                                    json={"phone_number": "09102222222",
+                                          "password": "@hamid14520"})
+        self.token = self.token = response.json()['token']['access']
+        self.headers = {"Authorization": f"Bearer {self.token}"}
+
+    @task
+    def get_user(self):
+        self.client.get('/api/user/user/', headers=self.headers)
+
+
+
 
