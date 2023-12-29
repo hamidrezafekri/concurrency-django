cca4bef9d10cb2b7cdab9b9204738d866b1399f5
hamidrezafekr <hamidrezafekri.dev@gmail.com>
2023-12-30 01:41:01 +0330

    add loadtest for buy and increase request

--- concurrency/loadtest/credit.py
+++ concurrency/loadtest/credit.py
@@ -1,38 +1,78 @@
-from locust import HttpUser, between
+from random import randint
 
-# from concurrency.credit.models import CreditRequest
+from locust import HttpUser, between, task
 
 
-class IncreaseCreditRequest(HttpUser):
-    between(1, 5)
+class SellAndBuyCheck(HttpUser):
+    between(1, 2)
 
     def on_start(self):
         response = self.client.post("/api/auth/jwt/seller-login/",
                                     json={"phone_number": "09101111111",
                                           "password": "@hamid14520"})
         self.token = self.token = response.json()['token']['access']
-        self.headers = {"Authorization": f"Bearer {self.token}"}
+        self.seller1_headers = {"Authorization": f"Bearer {self.token}"}
 
-    def create_increase_request(self):
-        self.client.post("api/credit/submit-request/",
-                         json={
-                             "amount": "100000"
-                         },
-                         headers=self.headers)
+        response = self.client.post("/api/auth/jwt/seller-login/",
+                                    json={"phone_number": "09103333333",
+                                          "password": "@hamid14520"})
+        self.token = self.token = response.json()['token']['access']
+        self.seller2_headers = {"Authorization": f"Bearer {self.token}"}
 
+        response = self.client.post("/api/auth/jwt/cutomer-login/",
+                                    json={"phone_number": "09102222222",
+                                          "password": "@hamid14520"})
+        self.token = self.token = response.json()['token']['access']
+        self.customer_headers = {"Authorization": f"Bearer {self.token}"}
 
-class ChangeRequestStatus(HttpUser):
-
-    def on_start(self):
         response = self.client.post("/api/auth/jwt/admin-login/",
                                     json={"phone_number": "09123456789",
                                           "password": "@hamid14520"})
         self.token = self.token = response.json()['token']['access']
-        self.headers = {"Authorization": f"Bearer {self.token}"}
+        self.admin_headers = {"Authorization": f"Bearer {self.token}"}
 
-    def change_credit_request_status(self):
-        request = self.client.put(f"/api/credit/change-request-status/",
-                                  json={
-                                      'status': True
-                                  },
-                                  headers=self.headers)
+    @task(1)
+    def increase_credit_seller1(self):
+        response = self.client.post("/api/credit/submit-request/",
+                                    json={
+                                        "amount": "100000"
+                                    },
+                                    headers=self.seller1_headers,
+                                    name="request_increase_credit")
+        self.request_id = response.json()['id']
+        self.client.put(f"/api/credit/change-request-status/{self.request_id}/",
+                        json={
+                            'status': True
+                        },
+                        headers=self.admin_headers, name="increase_credit-seller1")
+
+    @task(2)
+    def increase_credit_seller2(self):
+        response = self.client.post("/api/credit/submit-request/",
+                                    json={
+                                        "amount": "80000"
+                                    },
+                                    headers=self.seller2_headers,
+                                    name="request_increase_credit")
+        self.request_id = response.json()['id']
+        self.client.put(f"/api/credit/change-request-status/{self.request_id}/",
+                        json={
+                            'status': True
+                        },
+                        headers=self.admin_headers, name="increase_credit-seller2")
+
+    @task(3)
+    def buy_product(self):
+        product_id = randint(1, 3)
+        self.client.post("/api/credit/buy-product/",
+                         json={"product": product_id},
+                         headers=self.customer_headers,
+                         name='buy-product-seller1')
+
+    @task(4)
+    def buy_product(self):
+        product_id = randint(4, 7)
+        self.client.post("/api/credit/buy-product/",
+                         json={"product": product_id},
+                         headers=self.customer_headers,
+                         name='buy-product-seller2')
